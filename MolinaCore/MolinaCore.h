b5967daa098e39e3a5ac7adece1db61ff446b50c
Jaren Hamblin <hamblija@mla-33rrj12.molina.mhc>
2017-04-03 17:42:56 -0700

    Refactor

---
+++ MolinaCore/MolinaCore.h
@@ -0,0 +1,19 @@
+//
+//  MolinaCore.h
+//  MolinaCore
+//
+//  Created by Jaren Hamblin on 4/3/17.
+//  Copyright © 2017 Molina Healthcare, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MolinaCore.
+FOUNDATION_EXPORT double MolinaCoreVersionNumber;
+
+//! Project version string for MolinaCore.
+FOUNDATION_EXPORT const unsigned char MolinaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MolinaCore/PublicHeader.h>
+
+
